2398c18b3da109f66e27001ae3f5abe22045d270
Tran Diep Phuong Vy <andreatran2002@gmail.com>
2023-05-12 23:09:41 +0700

    update

--- app/src/main/java/ltdd/doan/mangxahoi/ui/view/fragment/ProfileFragment.java
+++ app/src/main/java/ltdd/doan/mangxahoi/ui/view/fragment/ProfileFragment.java
@@ -69,7 +69,7 @@ public class ProfileFragment extends Fragment {
         mViewModel.getUser().observe(getViewLifecycleOwner(), user -> {
             binding.setUser(user);
             // TODO: 4/18/2023 ảnh
-            if (!Objects.equals(user.getAvatar() , "none image")){
+            if (!Objects.equals(user.getAvatar() , "")){
                 Glide.with(requireContext())
                         .load(user.getAvatar() )
                         .into(binding.frgProfileUserAvatar);
